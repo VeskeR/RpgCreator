0f32ea8b70f2a10c39ed52dcca7e0515a20768a2
Andrey Bulat <andrii.bulat@gmail.com>
2016-05-23 00:02:49 +0300

    Added base class

---
+++ RpgCreator.DomainModel/Abstract/Base.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgCreator.DomainModel.Abstract
+{
+	class Base<T> where T : Base<T>
+	{
+		public Guid Id { get; }
+
+		public Base()
+		{
+			Id = Guid.NewGuid();
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			return GetHashCode() == obj.GetHashCode();
+		}
+	}
+}
