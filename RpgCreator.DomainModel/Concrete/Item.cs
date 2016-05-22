22195b5fefa9a5e733579a1a36c17d916876de8f
Andrey Bulat <andrii.bulat@gmail.com>
2016-05-23 01:40:33 +0300

    Added items classes

--- RpgCreator.DomainModel/Concrete/Item.cs
+++ RpgCreator.DomainModel/Concrete/Item.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using RpgCreator.DomainModel.Abstract;
 
 namespace RpgCreator.DomainModel.Concrete
 {
+    [DataContract]
     class Item : EntityBase<Item>
     {
+        [DataMember(Name = "ItemTypeId")]
+        private Guid _itemTypeId;
+
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public int Value { get; set; }
+
+        [IgnoreDataMember]
+        public ItemType ItemType => ItemType.Items[_itemTypeId];
+
+        public Item(string name, string description, int value, ItemType itemType)
+            :this(name, description, value, itemType.Id)
+        {
+            
+        }
+        public Item(string name, string description, int value, Guid itemTypeId)
+        {
+            Name = name;
+            Description = description;
+            Value = value;
+            _itemTypeId = itemTypeId;
+        }
     }
 }
--- RpgCreator.DomainModel/Concrete/ItemType.cs
+++ RpgCreator.DomainModel/Concrete/ItemType.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using RpgCreator.DomainModel.Abstract;
 
 namespace RpgCreator.DomainModel.Concrete
 {
+    [DataContract]
     class ItemType : EntityBase<ItemType>
     {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+
+        public ItemType(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
     }
 }
